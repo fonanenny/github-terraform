5a9737aa9e7647d99e52854742608e9dd09d7e43
fonanenny <fonanenny>
2023-12-09 18:32:25 +0100

    GRS TO LRS

--- leksjon07/staticwebsite/main.tf
+++ leksjon07/staticwebsite/main.tf
@@ -24,7 +24,7 @@ resource "azurerm_storage_account" "storage_account_web" {
   resource_group_name      = azurerm_resource_group.resource_group_web.name
   location                 = azurerm_resource_group.resource_group_web.location
   account_tier             = "Standard"
-  account_replication_type = "GRS"
+  account_replication_type = "LRS"
 
   static_website {
     index_document = var.index_document
