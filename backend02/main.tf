556817cdea9ce1a39728d52d4e9b0b8d28a2fdb9
fonanenny <fonanenny>
2023-12-09 18:01:46 +0100

    Change length randowm string

--- backend02/main.tf
+++ backend02/main.tf
@@ -40,7 +40,7 @@ resource "azurerm_storage_account" "storage_account_be" {
   resource_group_name      = azurerm_resource_group.resource_group_be.name
   location                 = azurerm_resource_group.resource_group_be.location
   account_tier             = "Standard"
-  account_replication_type = "LRS"
+  account_replication_type = "GRS"
 
  }
 
